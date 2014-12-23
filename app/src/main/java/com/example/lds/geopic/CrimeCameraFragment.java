eed6d71e5fd2b2188bfbff916503e5df86500132
lds56 <chenrui.qingqing@gmail.com>
2014-12-23 20:20:16 +0800

    Frame I

---
+++ app/src/main/java/com/example/lds/geopic/CrimeCameraFragment.java
@@ -0,0 +1,147 @@
+package com.example.lds.geopic;
+
+import android.annotation.TargetApi;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CrimeCameraFragment extends Fragment {
+    private static final String TAG = "CrimeCameraFragment";
+
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private View mProgressContainer;
+
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.test_layout, parent, false);
+
+        mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // deprecated, but required for pre-3.0 devices
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            public void surfaceCreated(SurfaceHolder holder) {
+                // tell the camera to use this surface as its preview area
+                try {
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // we can no longer display on this surface, so stop the preview.
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
+
+            public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            	if (mCamera == null) return;
+                // the surface has changed size; update the camera preview size
+                mCamera.setDisplayOrientation(90);
+                Camera.Parameters parameters = mCamera.getParameters();
+                //Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
+                Camera.Size s = getOptimalPreviewSize(parameters.getSupportedPreviewSizes(), w, h);
+                parameters.setPreviewSize(s.width, s.height);
+                Log.i(TAG, "chhose one: " + s.width + ", " + s.height);
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+        });
+        
+        return v; 
+    }
+
+    @TargetApi(9)
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {    
+            mCamera = Camera.open(0);
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+    private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
+        Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Size s : sizes) {
+            int area = s.width * s.height;
+            Log.i(TAG, s.width + ", " + s.height);
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.15;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+    
+}
